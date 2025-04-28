fd8c975cd1c7f282e3450cb0ee4c32a6db5eccb7
Marco Gerra <125428652+ilikeRocks0@users.noreply.github.com>
2025-04-27 23:20:39 -0700

    added everything including training

--- src/main.py
+++ src/main.py
@@ -3,23 +3,32 @@ import json
 import regex
 import spacy
 import re
+from reviewAI import ReviewAI
 
 #dont use this
-REVIEW_PATH = "Yelp-JSON\Yelp JSON\yelp_dataset\yelp_academic_dataset_review.json"
+REVIEW_PATH = "assets\Yelp-JSON\Yelp JSON\yelp_dataset\yelp_academic_dataset_review.json"
 #use this
 REVIEW_TEST_PATH = "src/test_review.txt"
-
-
+REVIEW_TEST_PATH2 = "src/test_review2.txt"
+REVIEW_RIGGED_TEST_PATH2 = "src/test_rigged_reviews.txt"
+NEW_REVIEW = "I WORST WORST PLACE!!!"
+#when to print updates
+PRINT_THRESHOLD = 100
 #gets the model to read do advanced clean up of the reviews
 nlp = spacy.load("en_core_web_sm")
 
 
 #goes through each review and stores it clean version in reviews
 def main():
+    
     #hold each word that will later be turned into a vector
     reviews, stars = proccess_reviews()
-    print(stars, reviews)
     
+    print("training AI...")
+    aiModel = ReviewAI(reviews, stars)
+    # print(stars, reviews)
+
+    print(aiModel.predict_new_review(clean_text(NEW_REVIEW)))
 
 
 #process review based on review test path
@@ -27,15 +36,29 @@ def main():
 def proccess_reviews():
     reviews = []
     stars = []
-    with open(REVIEW_TEST_PATH, encoding='utf-8') as f:
+    #counts what number we are reading
+    count = 0
+
+
+    with open(REVIEW_RIGGED_TEST_PATH2, encoding='utf-8') as f:
         line = f.readline()
         while (line != ""):
+            count+=1
             r = json.loads(line.strip())
             words = r["text"]
             goodness = int(r["stars"])
-            reviews.append(words)
-            stars.append(goodness)
+
+            #storing values
+            reviews.append(clean_text(words))
+            if goodness >= 4: # if stars is greater than or equal to 4 then its good
+                stars.append(1)
+            else:
+                stars.append(0)
+
             line = f.readline()
+
+            if(count % PRINT_THRESHOLD == 0):
+                print(f"read {count} reviews!")
     return reviews, stars
 
 
@@ -58,7 +81,7 @@ def clean_text(text) -> list:
             tokens.append(token.lemma_)
     
     
-    return tokens
+    return " ".join(tokens)
 
 
 
--- src/reviewAI.py
+++ src/reviewAI.py
@@ -3,20 +3,36 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+class ReviewAI:
+    vectorizer:CountVectorizer = None
+    model:LogisticRegression = None
 
-def review_reviewer(reviews: list[list[str]], stars:list[int]):
+    #sets up and trains the model
+    def __init__(self, reviews: list[list[str]], stars:list[int]):
 
-    #takes all unique words and sticks them into an vector
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(reviews)
-
-
-    X_train, X_test, y_train, y_test = train_test_split(X, stars, test_size=0.2)
-
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+        #takes all unique words and sticks them into an vector
+        self.vectorizer = CountVectorizer()
+        X = self.vectorizer.fit_transform(reviews)
 
 
-    y_pred = model.predict(X_test)
+        X_train, X_test, y_train, y_test = train_test_split(X, stars, test_size=0.8)
 
-    print(accuracy_score(y_test, y_pred))
+        self.model = LogisticRegression()
+        self.model.fit(X_train, y_train)
+
+        y_pred = self.model.predict(X_test)
+        print("Accurcy of model based on tests",accuracy_score(y_test, y_pred))
+
+        
+
+    #predicts new reivews
+    def predict_new_review(self, review:str):
+        if self.model == None or self.vectorizer == None:
+            print("ERROR: Model not trained object creation did not work")
+            return -1
+        
+        #need to put single item in a list
+        r_list = [review]
+
+        new_vector = self.vectorizer.transform(r_list)
+        return self.model.predict(new_vector)
